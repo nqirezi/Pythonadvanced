0f54b645588591b69ec1ddb8868e2cf49cdd57a2
nqirezi <144947863+nqirezi@users.noreply.github.com>
2025-11-13 19:11:29 +0100

    module20

---
+++ module20/fileJson.py
@@ -0,0 +1,20 @@
+{
+  "name": "John Doe",
+  "age": 28,
+  "adress": {
+    "Streat": "123 Main St",
+    "city": "Anytown",
+    "state": "CA",
+    "postal_code": "12345"
+  },
+  "contacts": [
+    {
+      "type": "email",
+      "value": "johndoe@gmail.com"
+    },
+     {
+      "type": "number",
+      "value": "j555-40-394"
+    }
+  ]
+}
